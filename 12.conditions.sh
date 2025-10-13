ad36c1137da5ad16d713b874f049fac1daa5d548
ramana sanku <sankuramana456@gmail.com>
2025-10-13 23:54:46 +0530

    this is varables

--- 12.conditions.sh
+++ 12.conditions.sh
@@ -2,9 +2,12 @@
 # writing a code for if a number is lessthan or equal to greater then 10
  NUMBER=$1
 
- if [ $NUMBER -le 10 ];
+ if [ $NUMBER -eq 10 ];
   then
-     echo " this number is less than : $NUMBER"
+     echo " this number is equal to  : $NUMBER"
+     elif [ $NUMBER -ge 10 ];
+     then 
+     echo "this number is greater than :: $NUMBER"
     else 
-echo "this number is greater than : $NUMBER"
+echo "this number is less than : $NUMBER"
 fi

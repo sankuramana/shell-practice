fb6aa83e9b4de5dcda1d9cc55a48193281f91df1
ramana sanku <sankuramana456@gmail.com>
2025-10-13 12:05:34 +0530

    this is 3rd sh

---
+++ 03-conversation1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this is conversation between two persons
+echo "ramesh :: hi suresh how are you?"
+echo  "suresh :: heloo ramesh how are you?"
+echo "ramesh  :: what are you learning ?"
+echo " suresh :: i am learning shell-script"

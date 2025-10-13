e9c36f63aaef2782e144982b6706b976362043ed
ramana sanku <sankuramana456@gmail.com>
2025-10-13 12:19:26 +0530

    this is varables

---
+++ 04-varaibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this is conversation between two persons
+person1=ramana
+person2=raju
+
+echo "$person :: hi $person2 how are you?"
+echo  "$person2 :: heloo $person how are you?"
+echo "$person  :: what are you learning ?"
+echo " $person2 :: i am learning shell-script"

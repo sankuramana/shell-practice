abf3e15588e47d0f959cace2161d556c508376fe
ramana sanku <sankuramana456@gmail.com>
2025-10-14 00:19:44 +0530

    this is varables

--- 13.read.cond.sh
+++ 13.read.cond.sh
@@ -2,7 +2,7 @@
 echo "please enter the number"
 read NUMBER
 
-if [ $($NUMBER % 2 -eq 0) ]; then 
+if [ $NUMBER % 2 -eq 0 ]; then 
 echo "the number  $NUMBER is EVEN"
 else 
 echo "the number $NUMBER is ODD"

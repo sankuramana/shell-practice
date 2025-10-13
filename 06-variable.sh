bd1fc10e84c10f40b92934f0e8e79233f753e3a7
ramana sanku <sankuramana456@gmail.com>
2025-10-13 14:27:41 +0530

    this is varables

---
+++ 06-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this is varables but new 
+person1=$1
+person2=$2
+
+echo "$person1 :: welcome to alaska puthin"
+echo "$person2 :: thank you $person1 not on finally not on zoom"
+echo "$person1 :: so dowe have deal "
+echo "$person2 :: if you make ukrain joins russia"

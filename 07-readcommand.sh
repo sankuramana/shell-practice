c8817e4c267e05e8d2f226421f877782c3a99c3a
ramana sanku <sankuramana456@gmail.com>
2025-10-13 14:45:24 +0530

    this is varables

--- 07-readcommand.sh
+++ 07-readcommand.sh
@@ -2,5 +2,5 @@
 #read in shellscript
 
 echo "pease enter your PIN number"
-read-s PIN_NUMBERZ #thisis the PIN_NUMBER is varable
+read -s PIN_NUMBERZ #thisis the PIN_NUMBER is varable
 echo "pin_number is $PIN_NUMBER"

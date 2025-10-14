e75cc10b234e550b8d77a54f61a3496ec34ea470
ramana sanku <sankuramana456@gmail.com>
2025-10-14 11:35:10 +0530

    this is varables

---
+++ 18.exit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#chceking if user is root or normal user and installing mysql and python and nginx
+
+  echo "this is not root user"
+  exit 0
+  echo "please use root user"
+  
+
+  

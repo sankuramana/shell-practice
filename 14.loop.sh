077f5417da97057ed385fb03bdb3a342082268e2
ramana sanku <sankuramana456@gmail.com>
2025-10-14 09:05:33 +0530

    this is varables

---
+++ 14.loop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#chceking if user is root or normal user
+  
+  USER=$(id -u)
+
+  if [ USER -eq 0 ]; then 
+  echo "proceed"
+  else 
+  echo "to install mysql run with root user"
+  dnf install mysql -y
+  fi

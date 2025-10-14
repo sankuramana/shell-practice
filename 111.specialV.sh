e89e76e4c6de1daa21ac04ad09f2ebc3d8ca8b8c
ramana sanku <sankuramana456@gmail.com>
2025-10-14 18:05:12 +0530

    this is varables

--- 111.specialV.sh
+++ 111.specialV.sh
@@ -4,5 +4,6 @@
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $#"
 echo "All variables passed to the script: $1,$2,$3,$4"
+echo "All variables passed to the script: $0"
 
 

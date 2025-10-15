a4a2b1ab4052be07accf40256a39db6d5794e306
ramana sanku <sankuramana456@gmail.com>
2025-10-16 01:56:06 +0530

    this is varables

--- 11.loopinstall.sh
+++ 11.loopinstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+LOG_FILE="/var/log/package_install.log"
 userid=$(id -u)
   if [ $userid -ne 0 ];
   then 
-  echo "please use roout user for installation"
+  echo "please use roout user for installation" >> $LOG_FILE
   exit 1
   fi
   install_packages()
@@ -11,15 +12,15 @@ userid=$(id -u)
     dnf list installed "$pakage"
     if [ $? -eq 0 ]; 
     then 
-    echo " $pakage installed already"
+    echo " $pakage installed already" >> $LOG_FILE
     else 
     dnf install $pakage -y
     fi 
     if [ $? -eq 0 ];
     then
-    echo " $pakage installed succuesfully "
+    echo " $pakage installed succuesfully " >> $LOG_FILE
     else
-    echo " $pakage installation is failed"
+    echo " $pakage installation is failed" >> $LOG_FILE
     fi
   }
   

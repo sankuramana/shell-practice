433720cc218738efc0bd0e69afb91ed7ac2c28fd
ramana sanku <sankuramana456@gmail.com>
2025-10-19 14:20:55 +0530

    this is roboshop

--- 01first.sh
+++ 01first.sh
@@ -4,3 +4,11 @@ echo "date is $DATE"
 a=10 b=2
 sum=$((a+b))
 echo "sum is $sum "
+#colors
+
+R='\033[0;31m'
+G='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e " $R today date is $DATE $N"
+echo -e "$R sum is $sum $N"
+

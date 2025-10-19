b3354a08063e9baf860f07e7e33c1599f850eae6
ramana sanku <sankuramana456@gmail.com>
2025-10-19 14:25:53 +0530

    this is roboshop

--- 01first.sh
+++ 01first.sh
@@ -10,11 +10,15 @@ R='\033[0;31m'
 G='\033[0;32m'
 NC='\033[0m' # No Color
 Y='\033[33m'
+BL='\033[0;34m'
 echo -e " $R today date is $DATE $N"
 echo -e "$R sum is $sum $N"
 echo -e "$NC sum is $sum $N"
 echo -e "$R sum is $sum $N"
 echo -e "$Y i am coming from madanapallli"
+echo -e "$Y practicing from bangalore today date us 19-10-2025$N"
+
+
 
 
 

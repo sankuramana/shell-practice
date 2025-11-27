d59ee01ff978569c6a418c331072f7f0ccf50b39
ramana sanku <sankuramana456@gmail.com>
2025-11-27 11:15:29 +0530

    shell

--- delet-files.sh
+++ delet-files.sh
@@ -3,7 +3,7 @@ mkdir -p $logs_folder
 script_name=$(echo $0)
 log_file="$logs_folder/$script_name.log"
 source_dir=/home/ec2-user/app-logs
-if [! -d $source_dir ];then
+if [ ! -d $source_dir ];then
 echo -e " -ERROR:: $source_dir dose not exit"
 exit 1
 fi
@@ -11,7 +11,7 @@ fi
 #filepath is the varialename
 files_to_delete=$(find $source_dir -name "*.log" -size +1c)
 
-while IFS read -r filepath
+while IFS=read -r filepath
 do
 echo "deleting the file: $filepath"
 

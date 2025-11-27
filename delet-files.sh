723549a61ed8cc421f4e7312766237868fbeebe8
ramana sanku <sankuramana456@gmail.com>
2025-11-27 11:32:27 +0530

    shell

--- delet-files.sh
+++ delet-files.sh
@@ -1,8 +1,9 @@
-logs_folder="var/log/shell-logs"
+logs_folder="/var/log/shell-logs"
 mkdir -p $logs_folder
 script_name=$(echo $0)
 log_file="$logs_folder/$script_name.log"
 source_dir=/home/ec2-user/app-logs
+#condtions for is folder exist or not
 if [ ! -d $source_dir ];then
 echo -e " -ERROR:: $source_dir dose not exit"
 exit 1
